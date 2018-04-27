5e62995bb619b352e67c5320bb18aeb56a5ed018
Karel Casier <karel.casier@thalmic.com>
2018-04-27 15:01:36 -0400

    std cppreference man pages

--- misc/install.sh
+++ misc/install.sh
@@ -1,14 +1,27 @@
 echo "› misc/install.sh"
 
+echo "› symlinking files"
 mkdir -p ~/.ctags.d/
 ln -s -f ${DOTFILES}/misc/defaults.ctags ~/.ctags.d/defaults.ctags
 
 ln -s -f ${DOTFILES}/misc/NeoVim.app /Applications/NeoVim.app
 
 #xcode command line tools
+echo "› installing xcode cmdline tools"
 if xcode-select --install 2>&1 | grep installed; then
   echo installed;
 else
   echo not installed, installing;
   xcode-select --install;
 fi
+
+echo "› installing cpp std man pages"
+# cppreference man pages
+current_dir=`pwd`
+temp_dir=`mktemp -d 2>/dev/null || mktemp -d -t 'stdman'`
+cd $tempdirectory
+git clone git@github.com:jeaye/stdman.git
+cd stdman
+./configure
+make install
+cd $current_dir
